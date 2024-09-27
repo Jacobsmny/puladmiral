94154203129e0662aaf9c7cc8d4f9e3fa8a7fddf
Jacobsmny <khursandkulov@gmail.com>
2024-09-27 10:41:43 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buyButton = document.getElementById('buy-button');
-    const purchaseForm = document.getElementById('purchase-form');
-
-    buyButton.addEventListener('click', () => {
-        purchaseForm.style.display = 'block';
-        purchaseForm.scrollIntoView({ behavior: 'smooth' });
-    });
-});
